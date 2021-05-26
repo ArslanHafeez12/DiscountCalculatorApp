c5838cd4fd0fbf7851960bfe12fadf40d2e270a1
arslanhafeez12 <arslanhaffez12@gmail.com>
2021-05-26 20:22:41 +0500

    Discount Calculation

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [originalPrice, setOriginalPrice] = useState('');
+  const [discountPercentage, setDiscountPercentage] = useState('');
+  const [finalPrice, setFinalPrice] = useState('');
+  const [save, setSave] = useState();
+
+  const calculateDiscount = () => {
+    if (originalPrice != '' && discountPercentage != '') {
+      if (discountPercentage >= 0 && discountPercentage <= 100 && originalPrice >= 0) {
+        const x = originalPrice / 100*discountPercentage
+        setFinalPrice(originalPrice-x)
+        setSave(x)
+      }
+      else {
+        setFinalPrice('')
+        setSave('')
+      }
+    }
+    else {
+      setFinalPrice('')
+      setSave('')
+    }
+  }
+  useEffect(() => {
+    calculateDiscount()
+  },[save,finalPrice,discountPercentage,originalPrice])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <view >
+        <Text >Original Price</Text>
+        <TextInput style={styles.inputText}
+          placeholder="Enter Original Price"
+          onChangeText={text => {
+            setOriginalPrice(text)
+            calculateDiscount()
+            
+          }}
+
+          defaultValue={originalPrice}
+        ></TextInput>
+      </view>
+      <view>
+        <Text>Discount Percentage</Text>
+        <TextInput style={styles.inputText}
+          placeholder="Enter Original Price"
+          onChangeText={text => {
+            setDiscountPercentage(text)
+            calculateDiscount()
+            
+          }}
+          defaultValue={discountPercentage}
+        ></TextInput>
+      </view>
+      <view>
+        <text>Final Price</text>
+        <text>{finalPrice}</text>
+      </view>
+      <view>
+        <text>You Save</text>
+        <text>{save}</text>
+      </view>
+
+      <button title="Chnage Name" onClick={calculateDiscount.bind(this, "Arslan")} >Press Me!!</button>
     </View>
   );
 }
@@ -18,4 +77,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputText: {
+    borderBottomWidth: 1,
+
+  }
 });
